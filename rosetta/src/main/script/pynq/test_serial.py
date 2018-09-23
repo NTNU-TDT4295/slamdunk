59f77b93ff6e94a694fb1f949c375cc1b4c835fd
Thomas Aven <avensin@hotmail.com>
2018-09-23 17:34:26 +0200

    Use retry interval to reconnect PYNQ to remote pointcloud.

--- rosetta/src/main/script/pynq/test_serial.py
+++ rosetta/src/main/script/pynq/test_serial.py
@@ -1,11 +1,36 @@
 import serial
 import socket
+import time
 
-port = serial.Serial("/dev/ttyUSB0", baudrate=115200)
-socket = socket.socket()
-socket.connect(('193.35.55.93', 6001))
+remote_ip_addr = '193.35.55.93'
+remote_port = 6001
+retry_interval = 3
 
-while True:
-    recv = port.read(8)
-    socket.sendall(recv)
-    print(recv)
+def connect():
+    new_socket = socket.socket()
+    while True:
+        try:
+            new_socket.connect((remote_ip_addr, remote_port))
+            break
+        except ConnectionRefusedError:
+            print('Could not connect to %s, retrying in %d seconds' % (remote_ip_addr, retry_interval))
+            time.sleep(retry_interval)
+    return new_socket
+
+def reflect(serial_port, socket):
+    while True:
+        recv = serial_port.read(8)
+        try:
+            socket.sendall(recv)
+            print(recv)
+        except BrokenPipeError:
+            return
+
+def main():
+    serial_port = serial.Serial('/dev/ttyUSB0', baudrate=115200)
+    while True:
+        socket = connect()
+        reflect(serial_port, socket)
+
+if __name__ == '__main__':
+    main()
