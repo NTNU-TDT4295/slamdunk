a8f888cfa04790d09c3fdae50b8a8c573138672a
Lasse Eggen <lasse.a.eggen@gmail.com>
2018-10-22 15:24:30 +0200

    SPI basic chisel peek/poke test.

---
+++ rosetta/src/test/scala/TestSPI.scala
@@ -0,0 +1,86 @@
+import Chisel._
+import rosetta._
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import RosettaTest._
+
+class TestSPISuite extends JUnitSuite {
+  @Test def AddTest {
+
+    // Tester-derived class to give stimulus and observe the outputs for the
+    // Module to be tested
+    class AddTest(c: SPI_Slave) extends Tester(c) {
+      // use peek() to read I/O output signal values
+      // peek(c.io.signature)
+      // // use poke() to set I/O input signal values
+      // poke(c.io.op(0), 10)
+      // poke(c.io.op(1), 20)
+      // // use step() to advance the clock cycle
+      // step(1)
+      // // use expect() to read and check I/O output signal values
+      // expect(c.io.sum, 10+20)
+
+      // setup
+      poke(c.io.spi_mosi, 0)
+      poke(c.io.spi_sck, 0)
+      poke(c.io.spi_ss, 1)
+      step(1)
+
+
+      printf("Reset complete\n--------------------\n\n\n")
+      // val sck = List(0,1,0,1,0,1,0)
+      val mosi = List(
+        0,1,0,0,0,0,0,1,
+        0,1,0,0,0,0,1,0,
+        0,1,0,0,0,0,1,1,
+        0,1,0,0,0,1,0,0,
+        0,1,0,0,0,1,0,1// ,
+        // 0,1,0,0,0,1,1,0,
+        // 0,1,0,0,0,1,1,1,
+        // 0,1,0,0,1,0,0,0,
+        // 0,1,0,0,1,0,0,1,
+        // 0,1,0,0,1,0,1,0,
+        // 0,1,0,0,1,0,1,1,
+        // 0,1,0,0,1,1,0,0,
+        // 0,1,0,0,1,1,0,1
+      ) // A->M
+
+      poke(c.io.spi_ss, 0) // select
+      for (i <- 0 until mosi.size) {
+
+        poke(c.io.spi_sck, 1)
+        poke(c.io.spi_mosi, mosi(i))
+        step(1)
+
+        poke(c.io.spi_sck, 0)
+        step(1)
+
+        // peek(c.byte_counter)
+        peek(c.word_data_received)
+        peek(c.word_received)
+        peek(c.byte_signal_received)
+        // peek(c.word_received)
+        // peek(c.bitcnt)
+        // peek(c.byte_counter)
+        // peek(c.write_adress)
+        // peek(c.word_data_received)
+      }
+
+    }
+
+    // Chisel arguments to pass to chiselMainTest
+    def testArgs = RosettaTest.stdArgs
+    // function that instantiates the Module to be tested
+    def testModuleInstFxn = () => { Module(new SPI_Slave()) }
+    // function that instantiates the Tester to test the Module
+    def testTesterInstFxn = c => new AddTest(c)
+
+    // actually run the test
+    chiselMainTest(
+      testArgs,
+      testModuleInstFxn
+    ) {
+      testTesterInstFxn
+    }
+  }
+}
