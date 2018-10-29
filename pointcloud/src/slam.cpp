3d961d646f8fce6a1ae5c45ba99f79fdfaa02ba1
Odd Kristian Kvarmestøl <okknor@gmail.com>
2018-10-29 12:52:38 +0100

    Move slamvis net functions to slam_common

--- pointcloud/src/slam.cpp
+++ pointcloud/src/slam.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <stdlib.h>
 #include <sys/socket.h>
-// #include "opengl.h"
 #include "utils.h"
 #include "slam_vis_net.h"
 
@@ -26,193 +25,6 @@ static struct timespec read_time() {
 	assert(!error);
 
 	return time;
-}
-
-static int send_packet_id(int fd, uint8_t packet_id) {
-	if (fd == -1) {
-		return -1;
-	}
-
-	ssize_t err;
-	err = send(fd, &packet_id, sizeof(packet_id), MSG_NOSIGNAL);
-
-	if (err < 0) {
-		return -1;
-	}
-
-	return 0;
-}
-
-static int send_map_tile_begin(int fd) {
-	return send_packet_id(fd, SLAM_PACKET_TILE_BEGIN);
-}
-
-static int send_map_tile_done(int fd) {
-	return send_packet_id(fd, SLAM_PACKET_TILE_DONE);
-}
-
-static int send_map_tile(int fd, float *map, size_t chunk_x, size_t chunk_y) {
-	if (fd == -1) {
-		return -1;
-	}
-
-	uint8_t buffer[(SLAM_MAP_TILE_SIZE*SLAM_MAP_TILE_SIZE)/4] = {0};
-
-	size_t map_offset =
-		(chunk_x * SLAM_MAP_TILE_SIZE) +
-		(chunk_y * SLAM_MAP_TILE_SIZE) * SLAM_MAP_WIDTH;
-
-	size_t map_row_stride = SLAM_MAP_WIDTH - SLAM_MAP_TILE_SIZE;
-
-	for (size_t i = 0; i < SLAM_MAP_TILE_SIZE*SLAM_MAP_TILE_SIZE; i++) {
-		uint8_t val;
-
-		size_t j = map_offset + i + (i / SLAM_MAP_TILE_SIZE) * map_row_stride;
-
-		if (hs_is_occupied(map[j])) {
-			val = 0x2;
-		} else if (hs_is_free(map[j])) {
-			val = 0x1;
-		} else {
-			val = 0x0;
-		}
-
-		buffer[i/4] |= val << ((j % 4)*2);
-	}
-
-	uint8_t header[3];
-
-	header[0] = SLAM_PACKET_TILE;
-	header[1] = chunk_x;
-	header[2] = chunk_y;
-
-	ssize_t err;
-	err = send(fd, header, sizeof(header), MSG_NOSIGNAL);
-
-	err = send(fd, buffer, sizeof(buffer), MSG_NOSIGNAL);
-	if (err < 0) {
-		perror("send");
-		return -1;
-	}
-
-	return 0;
-}
-
-static int send_full_map(int fd, float *map) {
-	if (fd == -1) {
-		return -1;
-	}
-
-	send_packet_id(fd, SLAM_PACKET_MAP);
-
-	uint8_t buffer[(SLAM_MAP_WIDTH*SLAM_MAP_HEIGHT)/4] = {0};
-
-	for (size_t i = 0; i < SLAM_MAP_WIDTH*SLAM_MAP_HEIGHT; i++) {
-		uint8_t val;
-		if (hs_is_occupied(map[i])) {
-			val = 0x2;
-		} else if (hs_is_free(map[i])) {
-			val = 0x1;
-		} else {
-			val = 0x0;
-		}
-		buffer[i/4] |= val << ((i % 4)*2);
-	}
-
-	size_t bytes_sent = 0;
-	ssize_t err;
-	while (bytes_sent < sizeof(buffer)) {
-		err = send(fd, buffer + bytes_sent,
-				   sizeof(buffer) - bytes_sent, MSG_NOSIGNAL);
-		if (err < 0) {
-			perror("send");
-			return - 1;
-		}
-		bytes_sent += err;
-	}
-
-	return 0;
-}
-
-static int send_map(int fd, float *map, unsigned int *map_last_update, unsigned int last_update) {
-	if (fd == -1) {
-		return -1;
-	}
-
-	constexpr size_t map_area =
-		SLAM_MAP_WIDTH * SLAM_MAP_HEIGHT;
-	constexpr size_t total_tiles =
-		SLAM_MAP_TILES_X * SLAM_MAP_TILES_Y;
-	constexpr size_t dirty_tiles_chunks =
-		(total_tiles / 64) +
-		(((total_tiles % 64) == 0) ? 0 : 1);
-
-	uint64_t dirty_tiles[dirty_tiles_chunks] = {0};
-	size_t num_dirty_tiles = 0;
-
-	for (size_t i = 0; i < map_area; i++) {
-		if (map_last_update[i] >= last_update) {
-			size_t tile_x = (i % SLAM_MAP_WIDTH) / SLAM_MAP_TILE_SIZE;
-			size_t tile_y = (i / SLAM_MAP_WIDTH) / SLAM_MAP_TILE_SIZE;
-			size_t tile_i = tile_x + tile_y * SLAM_MAP_TILES_X;
-
-			uint64_t mask = 1UL << (tile_i % 64);
-
-			// Only increment num_dirty_tiles if this tile was not
-			// already marked as dirty.
-			num_dirty_tiles += !(dirty_tiles[tile_i / 64] & mask);
-			dirty_tiles[tile_i / 64] |= mask;
-		}
-	}
-
-	if (num_dirty_tiles > total_tiles / 2) {
-		return send_full_map(fd, map);
-	} else if (num_dirty_tiles > 0) {
-		size_t err;
-		if (send_map_tile_begin(fd) < 0) {
-			return -1;
-		}
-
-		size_t tiles_updated = 0;
-
-		for (size_t i = 0; i < total_tiles; i++) {
-			if ((dirty_tiles[i / 64] & (1UL << (i % 64))) > 0) {
-				err = send_map_tile(fd, map, i % SLAM_MAP_TILES_X, i / SLAM_MAP_TILES_X);
-				tiles_updated += 1;
-				if (err < 0) {
-					return err;
-				}
-			}
-		}
-
-		if (send_map_tile_done(fd) < 0) {
-			return -1;
-		}
-	}
-
-	return 0;
-}
-
-int send_pose(int fd, vec3 pose) {
-	if (fd == -1) {
-		return -1;
-	}
-
-	uint8_t buffer[1 + 3*sizeof(int32_t)];
-	int32_t *pos_buffer = (int32_t *)&buffer[1];
-
-	buffer[0] = SLAM_PACKET_POSE;
-	pos_buffer[0] = pose.x * 1000.0f;
-	pos_buffer[1] = pose.y * 1000.0f;
-	pos_buffer[2] = pose.z * 1000000.0f;
-
-	ssize_t err;
-	err = send(fd, buffer, sizeof(buffer), MSG_NOSIGNAL);
-	if (err < (ssize_t)sizeof(buffer)) {
-		return -1;
-	}
-
-	return 0;
 }
 
 void init_slam(SlamContext &ctx) {
@@ -260,7 +72,7 @@ void tick_slam(SlamContext &ctx, const WindowFrameInfo &info) {
 				  ctx.points,
 				  ctx.numPoints);
 
-		send_pose(ctx.client_fd, ctx.slam.lastPosition);
+		slam_vis_send_pose(ctx.client_fd, ctx.slam.lastPosition);
 	}
 
 	struct timespec time_current;
@@ -278,10 +90,10 @@ void tick_slam(SlamContext &ctx, const WindowFrameInfo &info) {
 	}
 
 	if (ctx.client_fd >= 0 && ctx.last_sent_update < (unsigned int)ctx.slam.maps[0].currentUpdateIndex) {
-		if (send_map(ctx.client_fd,
-					 ctx.slam.maps[0].values,
-					 ctx.slam.maps[0].updateIndex,
-					 ctx.last_sent_update) == 0) {
+		if (slam_vis_send_map(ctx.client_fd,
+							  ctx.slam.maps[0].values,
+							  ctx.slam.maps[0].updateIndex,
+							  ctx.last_sent_update) == 0) {
 			ctx.last_sent_update = ctx.slam.maps[0].currentUpdateIndex;
 		} else {
 			ctx.client_fd = -1;
--- slam_common/src/slam_vis_net.h
+++ slam_common/src/slam_vis_net.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include "linmath.h"
+#include <stdint.h>
+#include <stddef.h>
+
 constexpr uint8_t SLAM_PACKET_MAP        = 0;
 constexpr uint8_t SLAM_PACKET_TILE_BEGIN = 1;
 constexpr uint8_t SLAM_PACKET_TILE       = 2;
@@ -14,3 +18,6 @@ constexpr float SLAM_MAP_METERS_PER_PIXEL = 0.025f;
 
 constexpr size_t SLAM_MAP_TILES_X = SLAM_MAP_WIDTH  / SLAM_MAP_TILE_SIZE;
 constexpr size_t SLAM_MAP_TILES_Y = SLAM_MAP_HEIGHT / SLAM_MAP_TILE_SIZE;
+
+int slam_vis_send_map(int fd, float *map, unsigned int *map_last_update, unsigned int last_update);
+int slam_vis_send_pose(int fd, vec3 pose);
