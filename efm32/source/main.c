f03cf9539457a5bb935af55a1e9c51ee1a494d2b
Thomas Aven <avensin@hotmail.com>
2018-11-03 09:51:06 +0100

    Update EFM32 main.c with current stable behaviour.

--- efm32/source/main.c
+++ efm32/source/main.c
@@ -27,8 +27,6 @@ int main(void)
 
 	// UART
 	init_uart();
-	/* GPIO_PinModeSet(gpioPortF, 8, gpioModePushPull, 1); //motoctl */
-
 
 	// I2C and the BNO055
 	init_i2c();
@@ -46,42 +44,28 @@ int main(void)
 	// Initalize GPIO interrupts for sonar
 	//init_sonar();
 
-	// Reset lidar completely to make it ready for scanning
-	unsigned int uart_channel = 0; // uart input channel
-
 	// Hold IMU data
-	uint8_t status_buf[1] = {7};
-	struct euler angles;
-	struct quaternion quat;
-	struct accel accelerations;
+	/* uint8_t status_buf[1] = {7}; */
+	/* struct euler angles; */
+	/* struct quaternion quat; */
+	/* struct accel accelerations; */
 
 	// Hold LIDAR data
-	size_t lidar_samples = 360;
-	uint8_t lidar_data[lidar_samples * 5];
-
+	/* size_t lidar_samples = 360; */
+	/* uint8_t lidar_data[lidar_samples * 5]; */
 	char lidar_sample;
 	/* init_lidar(false, 0); */
 
 	while (1) {
-		// 500 us per sample, 2000 samples per second, LIDAR
-		// SPI
-		/* get_samples_lidar(lidar_data, lidar_samples); */
-		/* put_uart_simple(0, lidar_data, lidar_samples*5); */
-		/* SPI_sendBuffer(lidar_data, lidar_samples*5); */
-		/* put_uart_simple(1, buffer, 255); */
+		while (!enable_lidar)
+			; // Busy wait, toggled by Top left button
 
-		/* while (!enable_lidar) */
-		/* 	; // Busy wait, toggled by Top left button */
-
-		/* lidar_sample = USART_Rx(UART0); */
+		lidar_sample = USART_Rx(UART0);
 		// /* put_uart_simple(1, lidar_sample, 1); */
-		/* SPI_sendBuffer(&lidar_sample, 1); */
+		SPI_sendBuffer(&lidar_sample, 1);
 
 		// TODO:
-		// toggle lidar
 		// sync sequence
-
-		/* continue; */
 
 		/* // IMU */
 		/* quat = get_quaternion_sample(); */
@@ -91,28 +75,17 @@ int main(void)
 		/* } */
 
 		// Fetch system status
-		performI2CRead(BNO055_I2C_ADDRESS, BNO055_SYS_STAT_ADDR, status_buf, 1);
-		put_uart_simple(1, status_buf, 1);
+		/* performI2CRead(BNO055_I2C_ADDRESS, BNO055_SYS_STAT_ADDR, status_buf, 1); */
+		/* put_uart_simple(1, status_buf, 1); */
 
-		DelayMs(100);
+		/* DelayMs(100); */
 
 		// Fetch samples for sensors available
-		angles = get_euler_sample();
-		quat = get_quaternion_sample();
-		accelerations = get_linear_acceleration_sample();
+		/* angles = get_euler_sample(); */
+		/* quat = get_quaternion_sample(); */
+		/* accelerations = get_linear_acceleration_sample(); */
 
 		// Send quaternion data
 		/* put_uart_simple(1, (uint8_t *) &quat.w, 8); */
-		uartPutData((uint8_t *) &quat.w, 8);
-
-		// Trigger the sonar, it will interrupt you
-		// trigger_sonar();
-
-		// Testing of accelerometer data
-		/* accelerations = get_linear_acceleration_sample(); */
-		/* uartPutData((uint8_t *) &accelerations.x, 6); */
 	}
-
-	// LEDS, (disabled for now, as they collide with UART)
-	/* leds_test(); */
 }
